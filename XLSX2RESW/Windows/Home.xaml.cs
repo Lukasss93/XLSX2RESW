ea85e88d1618efede8ee7ccff58b75dd0b609fff
Luca Patera <lucapatera@outlook.it>
2016-09-22 12:42:11 +0200

    Added dropzone

---
+++ XLSX2RESW/Windows/Home.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XLSX2RESW.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+            dropzone.AllowDrop = true;
+            dropzone.Drop += Dropzone_Drop;
+        }
+
+        private void Dropzone_Drop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                foreach(var file in files)
+                {
+                    Debug.WriteLine(file);
+                }
+            }
+        }
+    }
+}
